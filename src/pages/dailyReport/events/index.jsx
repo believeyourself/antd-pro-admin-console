ef458c091610c7116f325e7f96dad2378022af21
LiZeJun <634206380@qq.com>
2020-11-12 18:56:29 +0800

    事件漏斗适配

--- src/pages/dailyReport/events/index.jsx
+++ src/pages/dailyReport/events/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { PageContainer, } from '@ant-design/pro-layout';
 import { connect } from "umi";
 import { queryRoiReport } from './service';
-import { BackTop, Tabs, Button, DatePicker, Select, Table, Form } from 'antd';
+import { BackTop, Tabs, Button, DatePicker, Select, Table, Form, Row, Col } from 'antd';
 import DataSet from '@antv/data-set';
 import { Chart, Interval } from "bizcharts";
 import * as moment from "moment";
@@ -24,19 +24,8 @@ const TableList = (props) => {
   let [currentDate, setCurrentDate] = useState(moment.utc());
   let [data, setData] = useState({ events: [], mediaSource: [] });
   let [group, setGroup] = useState("events");
-
-  
-  const ds = new DataSet();
-  const dv = ds
-    .createView()
-    .source(data.events)
-    .transform({
-      type: 'percent',
-      field: 'event_count',
-      dimension: 'media_source',
-      groupBy: ['event_name'],
-      as: 'percent',
-    });
+  let [eventEnum, setEventEnum] = useState([]);
+  let [eventFunnel, setEventFunnel] = useState([]);
 
   let columns = {
     events: [
@@ -61,14 +50,20 @@ const TableList = (props) => {
     setLoading(true);
     let { data = {} } = await queryRoiReport(params);
     let { events, mediaSource } = data;
+    let eventEnum = [];
     let eventsData = [];
     let mediaSourceData = [];
     for (let event_name in events) {
       let index = 0;
+      if (!eventEnum.includes(event_name)) {
+        eventEnum.push(event_name);
+      }
+
       for (let media_source in events[event_name]) {
         eventsData.push({
           key: `${event_name}${media_source}`,
           event_name: index == 0 ? event_name : "",
+          event_name_origin: event_name,
           media_source,
           live_user_count: events[event_name][media_source].live_user_count,
           event_count: events[event_name][media_source].event_count
@@ -92,12 +87,37 @@ const TableList = (props) => {
     }
 
     setData({ events: eventsData, mediaSource: mediaSourceData });
+    setEventEnum(eventEnum);
     setLoading(false);
   }
 
   useEffect(() => {
     queryData();
-  }, [gameType])
+  }, [gameType]);
+
+  let eventFunnelData = [];
+  for (let i = 0; i < data.events.length; ++i) {
+    if (eventFunnel.includes(data.events[i].event_name_origin)) {
+      eventFunnelData.push(data.events[i]);
+    }
+  }
+
+  const ds = new DataSet();
+  const dv = ds
+    .createView()
+    .source(eventFunnelData)
+    .transform({
+      type: 'percent',
+      field: 'event_count',
+      dimension: 'media_source',
+      groupBy: ['event_name_origin'],
+      as: 'percent',
+    });
+
+  let eventOption = [];
+  for (let i = 0; i < eventEnum.length; ++i) {
+    eventOption.push(<Option key={eventEnum[i]} value={eventEnum[i]}>{eventEnum[i]}</Option>)
+  }
 
   return (
     <PageContainer>
@@ -110,14 +130,22 @@ const TableList = (props) => {
         </Form.Item>
       </Form>
       <div className={style.content}>
-        <h3>事件漏斗</h3>
+        <Row>
+          <Col xs={24} sm={24} md={12} lg={12}>
+            事件漏斗
+          </Col>
+          <Col xs={24} sm={24} md={12} lg={12}>
+            <Select allowClear onChange={setEventFunnel} placeholder="事件筛选" mode="multiple" className={style.event_select}>{eventOption}</Select>
+          </Col>
+        </Row>
         <Chart
+          placeholder
           autoFit height={300} data={dv.rows}>
-          <Interval adjust="stack" color="media_source" position="event_name*event_count" />
+          <Interval adjust="stack" color="media_source" position="event_name_origin*event_count" />
         </Chart>
       </div>
       <div className={style.content}>
-        <Tabs onChange={group => setGroup(group)} defaultValue="events">
+        <Tabs onBlur={group => setGroup(group)} defaultValue="events">
           <TabPane tab="按事件维度分组" key="events" ></TabPane>
           <TabPane tab="按媒体源分组" key="mediaSource"></TabPane>
         </Tabs>
